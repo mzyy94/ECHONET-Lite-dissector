762085acdfb93ae0903afdaa6f2931d1b5525dfa
Yuki MIZUNO <mizuyuu0904@gmail.com>
2016-06-20 11:35:30 +0900

    Add EHD subtree and show protocol info

--- echonet-lite.lua
+++ echonet-lite.lua
@@ -16,11 +16,25 @@
 echonetlite = Proto("echonet-lite", "ECHONET Lite")
 
 -- ========================================================
+-- ECHONET Lite Header value table
+-- ========================================================
+
+ehd1 = {
+    [0x10] = "ECHONET Lite Protocol"
+}
+
+ehd2 = {
+    [0x81] = "Format 1",
+    [0x82] = "Format 2"
+}
+
+-- ========================================================
 -- ECHONET Lite UDP payload fields definition.
 -- ========================================================
 
-echonetlite.fields.ehd1 = ProtoField.uint8("echonetlite.ehd1",  "EHD1", base.HEX)
-echonetlite.fields.ehd2 = ProtoField.uint8("echonetlite.ehd2",  "EHD2", base.HEX)
+echonetlite.fields.ehd  = ProtoField.protocol("echonetlite.ehd",  "ECHONET Lite Header (EHD)")
+echonetlite.fields.ehd1 = ProtoField.uint8("echonetlite.ehd1",  "EHD1", base.HEX, ehd1)
+echonetlite.fields.ehd2 = ProtoField.uint8("echonetlite.ehd2",  "EHD2", base.HEX, ehd2)
 echonetlite.fields.tid  = ProtoField.uint16("echonetlite.tid",  "TID",  base.HEX)
 echonetlite.fields.seoj = ProtoField.uint24("echonetlite.seoj", "SEOJ", base.HEX)
 echonetlite.fields.deoj = ProtoField.uint24("echonetlite.deoj", "DEOJ", base.HEX)
@@ -37,8 +51,9 @@ function echonetlite.dissector(buffer, pinfo, tree)
     pinfo.cols.info = ""
 
     local subtree = tree:add(echonetlite, buffer(0, data_len))
-    subtree:add(echonetlite.fields.ehd1, buffer(0, 1))
-    subtree:add(echonetlite.fields.ehd2, buffer(1, 1))
+    local ehdtree = subtree:add(echonetlite.fields.ehd, buffer(0, 2))
+    ehdtree:add(echonetlite.fields.ehd1, buffer(0, 1))
+    ehdtree:add(echonetlite.fields.ehd2, buffer(1, 1))
     subtree:add(echonetlite.fields.tid,  buffer(2, 2))
     subtree:add(echonetlite.fields.seoj, buffer(4, 3))
     subtree:add(echonetlite.fields.deoj, buffer(7, 3))
