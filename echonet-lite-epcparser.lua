8e2364d6a2d2001101a8ffec32ac6a4cd3a690fe
Yuki MIZUNO <mizuyuu0904@gmail.com>
2016-06-23 16:43:39 +0900

    Show property label in property tree

--- echonet-lite-epcparser.lua
+++ echonet-lite-epcparser.lua
@@ -20,13 +20,33 @@ local list = require("echonet-lite-codelist")
 
 local function epcparser(classgroup, class, epc, pdc, edt, tree, edata)
     if classgroup:uint() == 0x0e and class:uint() == 0xf0 then -- Node profile
-        if epc:uint() == 0xd5 or epc:uint() == 0xd6 then
-            if epc:uint() == 0xd5 then
-                tree:add(edata.fields.epc, epc, epc:uint(), nil, "(Instance list notification)")
-            else
-                tree:add(edata.fields.epc, epc, epc:uint(), nil, "(Self-node instance list S)")
-            end
+        if epc:uint() == 0x80 then
+            local label = "Operating status"
+            local state = {
+                [0x30] = "Booting",
+                [0x31] = "Not Booting"
+            }
+            tree:add(edata.fields.epc, epc, epc:uint(), nil, string.format("(%s)", label))
             tree:add(edata.fields.pdc, pdc)
+            tree:append_text(string.format(": %s", label))
+            if pdc:uint() == 0 then
+                do return end
+            end
+            local edttree = tree:add(edata.fields.edt, edt)
+            edttree:append_text(string.format(" (%s)", state[edt:uint()]))
+            tree:append_text(string.format(" = %s", state[edt:uint()]))
+            do return end
+        end
+        if epc:uint() == 0xd5 or epc:uint() == 0xd6 then
+            local label = ""
+            if epc:uint() == 0xd5 then
+                label = "Instance list notification"
+            else
+                label = "Self-node instance list S"
+            end
+            tree:add(edata.fields.epc, epc, epc:uint(), nil, string.format("(%s)", label))
+            tree:add(edata.fields.pdc, pdc)
+            tree:append_text(string.format(": %s", label))
             if pdc:uint() == 0 then
                 do return end
             end
@@ -43,8 +63,10 @@ local function epcparser(classgroup, class, epc, pdc, edt, tree, edata)
             do return end
         end
         if epc:uint() == 0xd7 then
-            tree:add(edata.fields.epc, epc, epc:uint(), nil, "(Self-node class list S)")
+            local label =  "Self-node class list S"
+            tree:add(edata.fields.epc, epc, epc:uint(), nil, string.format("(%s)", label))
             tree:add(edata.fields.pdc, pdc)
+            tree:append_text(string.format(": %s", label))
             if pdc:uint() == 0 then
                 do return end
             end
